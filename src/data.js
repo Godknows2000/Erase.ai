ff1f5c8fd64922557bb859e01c29fb809733b38f
Godknows2000 <92857706+Godknows2000@users.noreply.github.com>
2025-01-14 20:48:09 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,12 +4,3 @@ import { FaPython } from "react-icons/fa";
 import { SiFiverr } from "react-icons/si";
 import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
 import { SiFlutter, SiCsharp } from "react-icons/si";
-
-export const menu = [
-  { name: "About" },
-  { name: "Services" },
-  { name: "Skill" },
-  { name: "Projects" },
-  { name: "Testimonial" },
-  { name: "Contact" },
-];

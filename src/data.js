696267b8893092a71617195ee7306f0fdda40fe3
Godknows2000 <92857706+Godknows2000@users.noreply.github.com>
2025-01-14 20:48:29 +0200

    Delete data.js

--- src/data.js
+++
@@ -1,6 +0,0 @@
-import { DiBootstrap } from "react-icons/di";
-import { FaPython } from "react-icons/fa";
-
-import { SiFiverr } from "react-icons/si";
-import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
-import { SiFlutter, SiCsharp } from "react-icons/si";
